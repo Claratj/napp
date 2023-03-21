0e954d69f78a63fc9d86101a2cc0f99ecc195255
ClaraTJ <clara@claratoloba.com>
2023-03-21 12:01:24 +0100

    fix: prettier and eslint now working properly

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,24 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-  },
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: ['plugin:react/recommended', 'eslint:recommended', 'prettier'],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		ecmaFeatures: ~{
+			jsx: true,
+		},
+		sourceType: 'module',
+	},
+	plugins: ['react', 'prettier'],
+	rules: {
+		'react/react-in-jsx-scope': 'off',
+		'react/prop-types': 'off',
+		'indent': ['off', 'never'],
+		'allowIdentationTabs': 'off',
+		'prettier/prettier': 'error'
+	},
 };
