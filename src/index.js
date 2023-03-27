7494cbab571e929a94b5184a08823a44b36832eb
ClaraTJ <clara@claratoloba.com>
2023-03-27 17:20:27 +0200

    feat: update app, index, layout and routes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,10 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import CartProvider from './providers/CartProvider';
-import useInitialDataQueries from './queries/useInitialData.queries';
-import Router from './router/Router';
-import './sass/main.scss';
-
-// eslint-disable-next-line react-hooks/rules-of-hooks
-useInitialDataQueries();
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const queryClient = new QueryClient();
-
 root.render(
 	<React.StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<CartProvider>
-				<Router />
-			</CartProvider>
-		</QueryClientProvider>
+		<App />
 	</React.StrictMode>
 );
